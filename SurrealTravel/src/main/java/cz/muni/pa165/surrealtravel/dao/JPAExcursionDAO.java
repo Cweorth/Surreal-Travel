3bf62d5eb9e3f3827d60da44032dbaaa259820eb
Petr <Petr@147.251.44.244>
2014-10-12 17:19:02 +0200

    add exceptions in updateExcursion() and addExcursion()

--- SurrealTravel/src/main/java/cz/muni/pa165/surrealtravel/dao/JPAExcursionDAO.java
+++ SurrealTravel/src/main/java/cz/muni/pa165/surrealtravel/dao/JPAExcursionDAO.java
@@ -5,6 +5,7 @@
 package cz.muni.pa165.surrealtravel.dao;
 
 import cz.muni.pa165.surrealtravel.entity.Excursion;
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Objects;
 import javax.persistence.EntityManager;
@@ -33,6 +34,7 @@ public class JPAExcursionDAO implements ExcursionDAO {
         if(excursion.getId() < 0) throw new IllegalArgumentException("Excursion object is not valid: id < 0");
         if(excursion.getDescription().isEmpty()) throw new IllegalArgumentException("Description of excursion is empty string.");
         if(excursion.getDestination().isEmpty()) throw new IllegalArgumentException("Destination of excursion is empty string.");
+        if(excursion.getPrice().compareTo(BigDecimal.ZERO) < 0) throw new IllegalArgumentException("Excursion has a negative price");
         if(excursion.getPrice() == null) throw new NullPointerException("Excursion object is not valid: price is null");
         if(excursion.getExcursionDate() == null) throw new NullPointerException("Excursion object is not valid: excursionDate is null");
         if(excursion.getDuration() < 0) throw new IllegalArgumentException("Excursion object is not valid: Duration < 0");
@@ -70,6 +72,9 @@ public class JPAExcursionDAO implements ExcursionDAO {
         if(excursion.getDescription().isEmpty()) throw new IllegalArgumentException("Description of excursion is empty string.");
         if(excursion.getDestination().isEmpty()) throw new IllegalArgumentException("Destination of excursion is empty string.");
         if(excursion.getDuration() < 0) throw new IllegalArgumentException("Excursion object is not valid: Duration < 0");
+        if(excursion.getPrice().compareTo(BigDecimal.ZERO) < 0) throw new IllegalArgumentException("Excursion has a negative price");
+        if(excursion.getPrice() == null) throw new NullPointerException("Excursion object is not valid: price is null");
+        if(excursion.getExcursionDate() == null) throw new NullPointerException("Excursion object is not valid: excursionDate is null");
         
         return entityManager.merge(excursion);
     }
