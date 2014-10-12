d1b79d54018966c7fc07662aaa3e09dcccb05910
Petr <Petr@147.251.44.244>
2014-10-12 11:36:47 +0200

    JPACustomerDAOTest finished

--- SurrealTravel/src/test/java/cz/muni/pa165/surrealtravel/dao/JPACustomerDAOTest.java
+++ SurrealTravel/src/test/java/cz/muni/pa165/surrealtravel/dao/JPACustomerDAOTest.java
@@ -7,7 +7,6 @@ package cz.muni.pa165.surrealtravel.dao;
 import cz.muni.pa165.surrealtravel.AbstractTest;
 import cz.muni.pa165.surrealtravel.entity.Customer;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
@@ -16,7 +15,7 @@ import static org.junit.Assert.assertEquals;
 
 /**
  *
- * @author Petr
+ * @author Petr Dvořák [359819]
  */
 public class JPACustomerDAOTest extends AbstractTest {
     
@@ -86,7 +85,6 @@ public class JPACustomerDAOTest extends AbstractTest {
 //            .setParameter("name", name)
 //            .getResultList();
         
-        
         Assert.assertEquals(customers.size(),actual.size());
        
         em.getTransaction().begin();
@@ -95,5 +93,61 @@ public class JPACustomerDAOTest extends AbstractTest {
         em.remove(customer3);
         em.getTransaction().commit();
     }
+    
+    @Test
+    public void testUpdateCustomer() {
+        Customer customer1= mkcustomer("Josh","Brno");
+        Customer customer2= mkcustomer("Nick","Bratislava");
+        Customer customer3= mkcustomer("Jack","Praha");
         
+        em.getTransaction().begin();
+        em.persist(customer1);
+        em.persist(customer2);
+        em.persist(customer3);
+        em.getTransaction().commit();
+        
+        String name = customer1.getName();
+        String address = customer2.getAddress();
+        
+        em.getTransaction().begin();
+        customer3.setName(name);
+        customer3.setAddress(address);
+        em.persist(customer3);
+        em.getTransaction().commit();
+               
+        assertEquals(customer3.getName(),name);
+        assertEquals(customer3.getAddress(),address);
+        
+        em.getTransaction().begin();
+        em.remove(customer1);
+        em.remove(customer2);
+        em.remove(customer3);
+        em.getTransaction().commit();
+    }    
+        
+    @Test
+    public void testDeleteCustomer() {
+        Customer customer1= mkcustomer("Josh","Brno");
+        Customer customer2= mkcustomer("Josh","Bratislava");
+        Customer customer3= mkcustomer("Josh","Praha");
+        
+        em.getTransaction().begin();
+        em.persist(customer1);
+        em.persist(customer2);
+        em.persist(customer3);
+        em.getTransaction().commit();
+        
+        em.getTransaction().begin();
+        dao.deleteCustomer(customer2);
+        em.getTransaction().commit();
+       
+        String name = customer1.getName();
+        List<Customer> actual = dao.getCustomerByName(name);        
+        
+        Assert.assertEquals(dao.getAllCustomers(),actual);
+    }
+    
+    
  }
+
+
