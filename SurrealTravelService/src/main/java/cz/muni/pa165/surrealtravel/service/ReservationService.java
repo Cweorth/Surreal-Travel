70a5aa0dfd46e9122e9ef21faf7f12b5cff64020
kiclus <kiclus6@gmail.com>
2014-11-03 17:25:36 +0100

    reservationService completed...mising javadoc

--- SurrealTravelService/src/main/java/cz/muni/pa165/surrealtravel/service/ReservationService.java
+++ SurrealTravelService/src/main/java/cz/muni/pa165/surrealtravel/service/ReservationService.java
@@ -6,8 +6,13 @@
 package cz.muni.pa165.surrealtravel.service;
 
 import cz.muni.pa165.surrealtravel.dao.ReservationDAO;
+import cz.muni.pa165.surrealtravel.dto.CustomerDTO;
+import cz.muni.pa165.surrealtravel.dto.ExcursionDTO;
 import cz.muni.pa165.surrealtravel.dto.ReservationDTO;
+import cz.muni.pa165.surrealtravel.entity.Customer;
+import cz.muni.pa165.surrealtravel.entity.Excursion;
 import cz.muni.pa165.surrealtravel.entity.Reservation;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import org.dozer.DozerBeanMapper;
@@ -27,6 +32,7 @@ public class ReservationService {
   @Autowired
   private DozerBeanMapper mapper;
   
+  @Transactional
   public void addReservation(ReservationDTO reservationDTO){
       if (reservationDTO == null) {
             throw new IllegalArgumentException("reservation can't be null");
@@ -52,6 +58,62 @@ public class ReservationService {
       return result;
   }
   
+  public List<ReservationDTO> getAllReservationsByCustomer(CustomerDTO customerDTO){
+      List<ReservationDTO> result = new ArrayList<>();
+      //Reservation reservation=mapper.map(reservationDTO, Reservation.class);
+      Customer customer=mapper.map(customerDTO, Customer.class);
+      List<Reservation> reservations = reservationDAO.getAllReservationsByCustomer(customer);
+      for (Reservation reservation : reservations) {
+            result.add(mapper.map(reservation,ReservationDTO.class));
+        }
+      return result;
+  }
+  
+  public List<ReservationDTO> getAllReservationsByExcursion(ExcursionDTO excursionDTO){
+      List<ReservationDTO> result = new ArrayList<>();
+      Excursion excursion=mapper.map(excursionDTO, Excursion.class);
+      List<Reservation> reservations = reservationDAO.getAllReservationsByExcursion(excursion);
+      for (Reservation reservation : reservations) {
+            result.add(mapper.map(reservation,ReservationDTO.class));
+        }
+      return result;
+  }
+  
+  @Transactional
+  public void updateReservation(ReservationDTO reservationDTO){
+      if (reservationDTO == null) {
+            throw new IllegalArgumentException("reservationDTO to update can't be null");
+        }
+     validateReservation(reservationDTO);
+     Reservation reservation=mapper.map(reservationDTO, Reservation.class);
+     reservationDAO.updateReservation(reservation);
+      
+  }
+  
+  @Transactional
+  public void deleteReservation(ReservationDTO reservationDTO){
+      if (reservationDTO == null) {
+            throw new IllegalArgumentException("reservationDTO to update can't be null");
+        }
+      Reservation reservation=mapper.map(reservationDTO, Reservation.class);
+      reservationDAO.deleteReservation(reservation);
+  }
+  
+  @Transactional
+  public BigDecimal getFullPriceByCustomer(CustomerDTO customerDTO){
+      Customer customer=mapper.map(customerDTO, Customer.class);
+      BigDecimal result= reservationDAO.getFullPriceByCustomer(customer);
+      return result;
+  }
+  
+  @Transactional
+  public void removeExcursionFromAllReservations(ExcursionDTO excursionDTO){
+      if (excursionDTO == null) {
+            throw new IllegalArgumentException("excursionDTO to update can't be null");
+        }
+      Excursion excursion=mapper.map(excursionDTO, Excursion.class);
+      reservationDAO.removeExcursionFromAllReservations(excursion);
+  }
 
     private void validateReservation(ReservationDTO reservationDTO) {
         if (reservationDTO.getCustomer() == null) {
@@ -59,4 +121,13 @@ public class ReservationService {
         }
         
     }
+
+    public void setReservationDAO(ReservationDAO reservationDAO) {
+        this.reservationDAO = reservationDAO;
+    }
+
+    public void setMapper(DozerBeanMapper mapper) {
+        this.mapper = mapper;
+    }
+    
 }
