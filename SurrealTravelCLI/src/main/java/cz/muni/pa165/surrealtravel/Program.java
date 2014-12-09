f94be0f1ae7d92c3c63adb0319259b386b34e0c3
Cweorth <roman.lacko92@gmail.com>
2014-12-09 16:35:24 +0100

    Module skeleton for CLI and RestAPI

---
+++ SurrealTravelCLI/src/main/java/cz/muni/pa165/surrealtravel/Program.java
@@ -0,0 +1,13 @@
+package cz.muni.pa165.surrealtravel;
+
+/**
+ * Hello world!
+ *
+ */
+public class Program 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
