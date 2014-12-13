f57dfd6c362d0d3cc8e31aef670cff3af0b59fcc
dvori <dvori1@atlas.cz>
2014-12-13 19:43:39 +0100

    ExcursionRestController update + added curl examples into #31

--- SurrealTravelWeb/src/main/java/cz/muni/pa165/surrealtravel/rest/ExcursionRestController.java
+++ SurrealTravelWeb/src/main/java/cz/muni/pa165/surrealtravel/rest/ExcursionRestController.java
@@ -3,18 +3,19 @@ package cz.muni.pa165.surrealtravel.rest;
 import cz.muni.pa165.surrealtravel.dto.ExcursionDTO;
 import cz.muni.pa165.surrealtravel.service.ExcursionService;
 import java.util.List;
+import javassist.tools.rmi.ObjectNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import javassist.tools.rmi.ObjectNotFoundException;
+import org.springframework.web.bind.annotation.RestController;
 
 
 /**
@@ -52,35 +53,39 @@ public class ExcursionRestController {
     /**
      * Create new excursion
      * @param excursion
+     * @return 
      */
-    @RequestMapping(value="/create", method=RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value="/new", method=RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(value = HttpStatus.CREATED)
-    public @ResponseBody void addExcursion(ExcursionDTO excursion){
+    public @ResponseBody ExcursionDTO addExcursion(@RequestBody ExcursionDTO excursion){
         excursionService.addExcursion(excursion);
+        return excursion;
     }
     
     /**
-     * Get excursion by id.
+     * Modify excursion.
      * @param excursion
      * @param id
+     * @return 
      */
-    @RequestMapping(value = "/{id}", method = RequestMethod.PUT,  consumes = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/edit/{id}", method = RequestMethod.PUT,  consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(value = HttpStatus.ACCEPTED)
-    public @ResponseBody void updateExcursion(@PathVariable("id") Long id, ExcursionDTO excursion) {
+    public @ResponseBody ExcursionDTO updateExcursion(@PathVariable long id, @RequestBody ExcursionDTO excursion) {
         excursionService.updateExcursion(excursion);
+        return excursion;
     }
     
     /**
      * Delete excursion by id.
      * @param id
+     * @return 
+     * @throws javassist.tools.rmi.ObjectNotFoundException 
      */
-    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-    @ResponseStatus(value = HttpStatus.NO_CONTENT)
-    public void deleteExcursion(@PathVariable("id") Long id) throws ObjectNotFoundException {
-         ExcursionDTO excursion = excursionService.getExcursionById(id);
-           if (excursion == null) {
-                throw new ObjectNotFoundException("Excursion not found.");
-           }
-         excursionService.deleteExcursion(excursion);
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
+    public @ResponseBody ExcursionDTO deleteExcursion(@PathVariable long id) throws ObjectNotFoundException {
+        ExcursionDTO excursion = excursionService.getExcursionById(id);
+        if(excursion == null) throw new ObjectNotFoundException("Excursion not found.");
+        excursionService.deleteExcursionById(id);
+        return excursion;
     }
 }
