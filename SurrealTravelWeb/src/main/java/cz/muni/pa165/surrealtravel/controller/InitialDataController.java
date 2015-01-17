1b67b70bbbf1333a127edb21b120b089dab29bd5
Cweorth <roman.lacko92@gmail.com>
2015-01-17 12:12:39 +0100

    Moved init() content from TripController.java to InitialDataControler.java

--- SurrealTravelWeb/src/main/java/cz/muni/pa165/surrealtravel/controller/InitialDataController.java
+++ SurrealTravelWeb/src/main/java/cz/muni/pa165/surrealtravel/controller/InitialDataController.java
@@ -17,7 +17,10 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.EnumSet;
 import java.util.GregorianCalendar;
+import java.util.List;
 import javax.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -45,6 +48,38 @@ public class InitialDataController {
     
     @Autowired
     private ReservationService reservationService;
+    
+    private static final Logger logger = LoggerFactory.getLogger(InitialDataController.class);
+    
+    private Date mkdate(int day, int month, int year) {
+        Calendar calendar = new GregorianCalendar();
+        calendar.set(year, month, day, 0, 0, 0);
+        return calendar.getTime();
+    }    
+    
+    private ExcursionDTO mkexcursion(Date date, int duration, String description, String destination, BigDecimal price) {
+        ExcursionDTO excursion = new ExcursionDTO();
+        excursion.setExcursionDate(date);
+        excursion.setDuration(duration);
+        excursion.setDescription(description);
+        excursion.setDestination(destination);
+        excursion.setPrice(price);
+        
+        logger.info(String.format("[mk] Created %s", excursion.toString()));
+        return excursion;
+    }
+    
+    private TripDTO mktrip(Date from, Date to, String destination, int capacity, BigDecimal price) {
+        TripDTO trip = new TripDTO();
+        trip.setDateFrom(from);
+        trip.setDateTo(to);
+        trip.setDestination(destination);
+        trip.setCapacity(capacity);
+        trip.setBasePrice(price);
+        
+        logger.info(String.format("[mk] Created %s", trip.toString()));
+        return trip;
+    }        
     
     @PostConstruct
     public void init() {
@@ -112,6 +147,30 @@ public class InitialDataController {
         excursionService.addExcursion(e1);
         excursionService.addExcursion(e2);
         excursionService.addExcursion(e3);
+
+        List<ExcursionDTO> excursions = Arrays.asList(
+            mkexcursion(mkdate(20, 10, 2941), 2, "Battle of Five Armies",   "Erebor",      new BigDecimal(500)),
+            mkexcursion(mkdate(25, 10, 3018), 1, "Council of Elrond",       "Rivendell",   new BigDecimal(150)),
+            mkexcursion(mkdate(02, 03, 3019), 1, "Destruction of Isengard", "Isengard",    new BigDecimal(400)),
+            mkexcursion(mkdate(03, 03, 3019), 1, "Battle of Hornburg",      "Helm's Deep", new BigDecimal(350)),
+            mkexcursion(mkdate(14, 03, 3019), 3, "Mt Doom Excursion",       "Mordor",      new BigDecimal(200)),
+            mkexcursion(mkdate(25, 03, 3019), 2, "Downfall of Barad-dûr",   "Mordor",      new BigDecimal(300))
+        );
+        
+        for(ExcursionDTO e : excursions) {
+            excursionService.addExcursion(e);
+        }
+        
+        List<TripDTO> trips = Arrays.asList(
+            mktrip(mkdate(11, 10, 2960), mkdate(18, 11, 2960), "Wonderful Moria",   20, new BigDecimal(800)),
+            mktrip(mkdate(24, 10, 3018), mkdate(25, 03, 3019), "The Life of Frodo", 45, new BigDecimal(900))
+        );
+        
+        trips.get(1).setExcursions(excursions.subList(1, 5));
+        
+        for(TripDTO t : trips) {
+            tripService.addTrip(t);
+        }
         
         // Available trips
         // TODO, there is just one for reservation, can be removed l8r
@@ -135,11 +194,5 @@ public class InitialDataController {
         SecurityContextHolder.getContext().getAuthentication().setAuthenticated(false);
 
     }
-    
-    private Date mkdate(int day, int month, int year) {
-        Calendar calendar = new GregorianCalendar();
-        calendar.set(year, month, day, 0, 0, 0);
-        return calendar.getTime();
-    } 
     
 }
