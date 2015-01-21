f29689405323b18c56f8ab7c48c51674fbaf5f96
kiclus <kiclus6@gmail.com>
2015-01-22 00:22:16 +0100

    Testing Problem2

--- SurrealTravelDAO/src/test/java/cz/muni/pa165/surrealtravel/dao/JPAAccountDAOTest.java
+++ SurrealTravelDAO/src/test/java/cz/muni/pa165/surrealtravel/dao/JPAAccountDAOTest.java
@@ -20,7 +20,6 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 
 
