fa787b5294d9cd5c1ad32ea14826301e55589aaa
kiclus <kiclus6@gmail.com>
2015-01-22 21:57:09 +0100

    completed tests method without null testing

--- SurrealTravelDAO/src/test/java/cz/muni/pa165/surrealtravel/dao/JPAAccountDAOTest.java
+++ SurrealTravelDAO/src/test/java/cz/muni/pa165/surrealtravel/dao/JPAAccountDAOTest.java
@@ -9,42 +9,44 @@ import cz.muni.pa165.surrealtravel.AbstractPersistenceTest;
 import cz.muni.pa165.surrealtravel.dto.UserRole;
 import cz.muni.pa165.surrealtravel.entity.Account;
 import cz.muni.pa165.surrealtravel.entity.Customer;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import javax.persistence.EntityManager;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import org.junit.Assert;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 
 /**
  *
- * @author Kiclus
+ * @author Tomáš Kácel [359965]
  */
-public class JPAAccountDAOTest extends AbstractPersistenceTest  {
+@Transactional
+public class JPAAccountDAOTest  extends AbstractPersistenceTest {
     
     public JPAAccountDAOTest() {
     }
     
   @Autowired
   private AccountDAO accountDao;
+  
+  @Autowired
+  private CustomerDAO customerDao;
+  
+ 
     
     
-    /**
-     * Test of addAccount method, of class JPAAccountDAO.
-     */
+   
     @Test
     public void testAddAccount() {
-        Customer customer = mkcustomer("Some dude", "not here");
+        Customer customer = mkcustomer("Some girls", "there");
         Account act= new Account();
         act.setCustomer(customer);
-        //act.setId(id);
         act.setPassword("pass");
         act.setPlainPassword("pass");
         act.setUsername("Karel");
@@ -53,10 +55,172 @@ public class JPAAccountDAOTest extends AbstractPersistenceTest  {
         act.setRoles(r);
         
         accountDao.addAccount(act);
-        assertTrue(act.getId() > 0);
-        //Account retrieved = em.find(Account.class, act.getId());
-        //assertEquals(act, retrieved);
+        Account retrieved = em.find(Account.class, act.getId());
+        assertEquals(act, retrieved);
     }
+    
+    @Test
+    public void getAccountById() {
+        Customer customer = mkcustomer("Some dude", "not here");
+        Account act= new Account();
+        act.setCustomer(customer);
+        act.setPassword("pass");
+        act.setPlainPassword("pass");
+        act.setUsername("Karel");
+        Set<UserRole> r= new HashSet<>();
+        r.add(UserRole.ROLE_USER);
+        act.setRoles(r);
+        accountDao.addAccount(act);
+        long id = act.getId();
+        Account retriew = accountDao.getAccountById(id);
+        assertEquals(act, retriew);
+    }
+    
+    @Test
+    public void getAccountByUsername() {
+        Customer customer = mkcustomer("Some dudes", "here");
+        Account act= new Account();
+        act.setCustomer(customer);
+        act.setPassword("pass");
+        act.setPlainPassword("pass");
+        act.setUsername("Karel");
+        Set<UserRole> r= new HashSet<>();
+        r.add(UserRole.ROLE_USER);
+        act.setRoles(r);
+        customerDao.addCustomer(customer);
+        accountDao.addAccount(act);
+        
+        Account retrieved=accountDao.getAccountByUsername(act.getUsername());
+        assertEquals(act, retrieved);
+        
+        
+    }
+    
+    @Test
+    public void getAccountByCustomer() {
+        Customer customer = mkcustomer("Peter", "Brno");
+        Account act= new Account();
+        act.setCustomer(customer);
+        act.setPassword("pass");
+        act.setPlainPassword("pass");
+        act.setUsername("Petr");
+        Set<UserRole> r= new HashSet<>();
+        r.add(UserRole.ROLE_USER);
+        act.setRoles(r);
+        customerDao.addCustomer(customer);
+        accountDao.addAccount(act);
+        List<Account> retrieved= accountDao.getAccountByCustomer(customer);
+        if(retrieved.size() !=1){
+        fail();
+        }
+        else{
+            assertEquals(act, retrieved.get(0));
+        }
+    }
+    
+    @Test
+    public void getAllAccounts() {
+      List<Account> list= prepareAccountBatch();
+      for(Account act:list){
+          customerDao.addCustomer(act.getCustomer());
+          accountDao.addAccount(act);
+      }
+      List<Account> retrieved= accountDao.getAllAccounts();
+        //assertEquals(list.size(), retrieved.size());
+      for(Account account:list){
+          Assert.assertTrue(retrieved.contains(account));
+      }
+        
+    }
+    
+    @Test
+     public void deleteAccountById() {
+         List<Account> acts= prepareAccountBatch();
+         customerDao.addCustomer(acts.get(0).getCustomer());
+         accountDao.addAccount(acts.get(0));
+         Long id= acts.get(0).getId();
+         accountDao.deleteAccountById(id);
+         Assert.assertFalse(accountDao.getAllAccounts().contains(acts.get(0)));    
+    }
+     
+    @Test 
+    public void updateAccount() {
+        List<Account> acts= prepareAccountBatch();
+        for(Account act:acts){
+          customerDao.addCustomer(act.getCustomer());
+          accountDao.addAccount(act);
+      }
+      List<Account> actsBeforeUpdate= accountDao.getAllAccounts();
+      Set<UserRole> r= new HashSet<>();
+      r.add(UserRole.ROLE_ROOT);
+      for(Account account: actsBeforeUpdate){
+          
+          account.setRoles(r);
+          accountDao.updateAccount(account);
+      }
+      List<Account> actsAfterUpdate= accountDao.getAllAccounts();
+      
+      for(Account a : actsAfterUpdate) {
+            Assert.assertEquals(r, a.getRoles());
+        }
+        
+    } 
+     
+     
+    /*
+    * prepare list of Account for some testing
+    * @return list of account
+    */
+    private List<Account> prepareAccountBatch(){
+        List<Account> list= new ArrayList<>();
+        Customer customer = mkcustomer("Peters", "Brno");
+        Account act= new Account();
+        act.setCustomer(customer);
+        act.setPassword("pass");
+        act.setPlainPassword("pass");
+        act.setUsername("Petr3");
+        Set<UserRole> r= new HashSet<>();
+        r.add(UserRole.ROLE_USER);
+        act.setRoles(r);
+        
+        Customer customer2 = mkcustomer("Karel", "Praha");
+        Account act2= new Account();
+        act2.setCustomer(customer);
+        act2.setPassword("pass");
+        act2.setPlainPassword("pass");
+        act2.setUsername("Karel5");
+        Set<UserRole> r2= new HashSet<>();
+        r2.add(UserRole.ROLE_STAFF);
+        act2.setRoles(r2);
+        
+        Customer customer3 = mkcustomer("Karel", "Prerov");
+        Account act3= new Account();
+        act3.setCustomer(customer);
+        act3.setPassword("pass");
+        act3.setPlainPassword("pass");
+        act3.setUsername("Karel12");
+        Set<UserRole> r3= new HashSet<>();
+        r3.add(UserRole.ROLE_ADMIN);
+        act3.setRoles(r3);
+        list.add(act);
+        list.add(act2);
+        list.add(act3);
+        return list;
+        
+    }
+
+    
+
+   
+    
+    
+    
+    
+
+  
+    
+    
+    
 
     
     
