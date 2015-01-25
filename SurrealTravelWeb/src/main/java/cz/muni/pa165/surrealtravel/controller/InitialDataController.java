e94e7c2fdf40dbc209b6bda9e43a2d3f2584e2f2
Cweorth <roman.lacko92@gmail.com>
2015-01-25 17:26:04 +0100

    New demo entities

--- SurrealTravelWeb/src/main/java/cz/muni/pa165/surrealtravel/controller/InitialDataController.java
+++ SurrealTravelWeb/src/main/java/cz/muni/pa165/surrealtravel/controller/InitialDataController.java
@@ -52,58 +52,55 @@ public class InitialDataController {
     private static final Logger                logger  = LoggerFactory.getLogger(InitialDataController.class);
     private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
     
+    private List<ExcursionDTO>    excursions;
+    private List<TripDTO>         trips;
+    private List<CustomerDTO>     customers;
+    private List<ReservationDTO>  reservations;
+    private List<AccountDTO>      accounts;
+    
     private Date mkdate(int day, int month, int year) {
         Calendar calendar = new GregorianCalendar();
         calendar.set(year, month, day, 0, 0, 0);
         return calendar.getTime();
     }    
+            
+    private ExcursionDTO mkexcursion(Date date, int duration, String description, String destination, int price) {
+        ExcursionDTO excursion = new ExcursionDTO();
+        excursion.setExcursionDate(date);
+        excursion.setDuration(duration);
+        excursion.setDescription(description);
+        excursion.setDestination(destination);
+        excursion.setPrice(new BigDecimal(price));
+        
+        logger.info("[init-mk] excursion: " + excursion.toString());
+        return excursion;
+    }
+    
+    private TripDTO mktrip(Date from, Date to, String destination, int capacity, int price, int... excursionIxs) {
+        TripDTO trip = new TripDTO();
+        trip.setDateFrom(from);
+        trip.setDateTo(to);
+        trip.setDestination(destination);
+        trip.setCapacity(capacity);
+        trip.setBasePrice(new BigDecimal(price));
+        
+        for(int eid : excursionIxs) {
+            trip.addExcursion(excursions.get(eid));
+        }
+        
+        logger.info("[init-mk] trip: " + trip.toString());
+        return trip;
+    }
     
     private CustomerDTO mkcustomer(String name, String address) {
         CustomerDTO customer = new CustomerDTO();
         customer.setName(name);
         customer.setAddress(address);
         
-        logger.info("[init-mk] created " + customer.toString());
+        logger.info("[init-mk] customer: " + customer.toString());
         return customer;
-    }    
-    
-    private ExcursionDTO mkexcursion(Date date, int duration, String description, String destination, BigDecimal price) {
-        ExcursionDTO excursion = new ExcursionDTO();
-        excursion.setExcursionDate(date);
-        excursion.setDuration(duration);
-        excursion.setDescription(description);
-        excursion.setDestination(destination);
-        excursion.setPrice(price);
-        
-        logger.info("[init-mk] created " + excursion.toString());
-        return excursion;
-    }
-    
-    private TripDTO mktrip(Date from, Date to, String destination, int capacity, BigDecimal price) {
-        TripDTO trip = new TripDTO();
-        trip.setDateFrom(from);
-        trip.setDateTo(to);
-        trip.setDestination(destination);
-        trip.setCapacity(capacity);
-        trip.setBasePrice(price);
-        
-        logger.info("[init-mk] created " + trip.toString());
-        return trip;
-    }
+    }        
 
-    private ReservationDTO mkreservation(CustomerDTO customer, TripDTO trip, List<ExcursionDTO> excursions) {
-        ReservationDTO reservation = new ReservationDTO();
-        reservation.setCustomer(customer);
-        reservation.setTrip(trip);
-        
-        if (excursions != null) {
-            reservation.setExcursions(excursions);
-        }
-        
-        logger.info("[init-mk] created " + reservation.toString());
-        return reservation;
-    }
-    
     private AccountDTO mkaccount(String username, String password, UserRole role, CustomerDTO customer) {
         AccountDTO account = new AccountDTO();
         account.setUsername(username);
@@ -112,151 +109,169 @@ public class InitialDataController {
         account.setRoles(EnumSet.of(role));
         account.setCustomer(customer);
         
-        logger.info("[init-mk] created " + account.toString());
+        logger.info("[init-mk] account: " + account.toString());
         return account;
+    }
+    
+    private AccountDTO mkaccount(String username, String password, UserRole role, int customerIx) {
+        return mkaccount(username, password, role, customers.get(customerIx));
+    }    
+    
+    private ReservationDTO mkresrv(int customerIx, int tripIx, int... excursionIxs) {
+        ReservationDTO reservation = new ReservationDTO();
+        reservation.setCustomer(customers.get(customerIx));
+        reservation.setTrip(trips.get(tripIx));
+        
+        for (int ix : excursionIxs) {
+            reservation.getExcursions().add(excursions.get(ix));
+        }
+    
+        logger.info("[init-mk] reservation: " + reservation.toString());
+        return reservation;
+    }
+    
+    private int[] range(int from, int to) {
+        int[] result = new int[to - from + 1];
+        for (int ix = 0; ix < result.length; ++ix) {
+            result[ix] = ix + from;
+        }
+        
+        return result;
     }
     
     @PostConstruct
     public void init() {
         // Because we are using secured service methods, we need to do authentication.
         // User "root" is created by hibernate on DAO layer (see import.sql).
+        // All of the following code is therefore done using root's credentials.
         UsernamePasswordAuthenticationToken authRequest = new UsernamePasswordAuthenticationToken("root", "root");
         SecurityContextHolder.getContext().setAuthentication(authRequest);
 
-        // Available customers
-        CustomerDTO c1 = new CustomerDTO();
-        CustomerDTO c2 = new CustomerDTO();
-        CustomerDTO c3 = new CustomerDTO();
-        c1.setName("Twoflower");
-        c1.setAddress("Discworld");
-        c2.setName("Luggage");
-        c2.setAddress("Discworld");
-        c3.setName("Slash");
-        c3.setAddress("City of Angels");
-        customerService.addCustomer(c1);
-        customerService.addCustomer(c2);
-        customerService.addCustomer(c3);
+        //----------------------------------------------------------------------
+        //  Core accounts (except root)
+        //----------------------------------------------------------------------
         
-        // Available accounts
-        String passwd = "lysa had poisoned me";
+        CustomerDTO pa165 = mkcustomer("PA165", "Faculty of Informatics, Masaryk University, Brno");
+        customerService.addCustomer(pa165);
+        accountService.addAccount(mkaccount("rest",  "rest",  UserRole.ROLE_STAFF,  null));
+        accountService.addAccount(mkaccount("pa165", "pa165", UserRole.ROLE_ADMIN, pa165));
         
-        AccountDTO account = new AccountDTO();
-        account.setUsername("johnarryn");
-        account.setPassword(encoder.encode(passwd));
-        account.setPlainPassword(passwd);
+        //----------------------------------------------------------------------
+        //  Excursions
+        //----------------------------------------------------------------------
         
-        account.setRoles(EnumSet.of(UserRole.ROLE_ADMIN));
-        accountService.addAccount(account);
+        excursions = Arrays.asList(
+        // The Lord of the Rings
+        /* 00 */mkexcursion(mkdate(20, 10, 2941),  2, "Battle of Five Armies",              "Erebor",         1500),
+        /* 01 */mkexcursion(mkdate( 1,  2, 3017),  3, "Flora and Fauna of Dead Marches",    "Dead Marches",    899),
+        /* 02 */mkexcursion(mkdate(25, 10, 3018),  1, "Council of Elrond",                  "Rivendell",      2799),
+        /* 03 */mkexcursion(mkdate( 2,  3, 3019),  1, "Destruction of Isengard",            "Isengard",        799),
+        /* 04 */mkexcursion(mkdate( 3,  3, 3019),  1, "Battle of Hornburg",                 "Helm's Deep",    2350),
+        /* 05 */mkexcursion(mkdate(14,  3, 3019),  3, "Mt Doom Excursion",                  "Mordor",          899),
+        /* 06 */mkexcursion(mkdate(14,  3, 3019), 12, "The Siege of Minas Tirith",          "Minas Tirith",   1300),
+        /* 07 */mkexcursion(mkdate(25,  3, 3019),  2, "Downfall of Barad-dûr",              "Mordor",          999),
+        /* 08 */mkexcursion(mkdate( 1,  5, 3019),  2, "Coronation of King Aragorn Elessar", "Minas Tirith",   4300),
+        /* 09 */mkexcursion(mkdate(10,  7, 3019),  1, "The Funeral of Théoden",             "Edoras",         3299),
         
-        String password = "pa165";
-        AccountDTO a1 = new AccountDTO();
-        AccountDTO a2 = new AccountDTO();
-        a1.setUsername("pa165");
-        a1.setPassword(encoder.encode(password));
-        a1.setPlainPassword(password);
-        a1.setRoles(EnumSet.of(UserRole.ROLE_ADMIN));
-        password = "password";
-        a2.setUsername("username");
-        a2.setPassword(encoder.encode(password));
-        a2.setPlainPassword(password);
-        a2.setRoles(EnumSet.of(UserRole.ROLE_USER));
-        a2.setCustomer(c1);
-        accountService.addAccount(a1);
-        accountService.addAccount(a2);
+        // Game of Thrones
+        /* 10 */mkexcursion(mkdate( 7,  1,    2),  1, "Aegon's Landing",                    "King's Landing", 1700),
+        /* 11 */mkexcursion(mkdate(14,  9,  130),  9, "Dance over Harrenhal (w/ Dragons)",  "Harrenhal",      2699),
+        /* 12 */mkexcursion(mkdate( 3,  1,  299),  2, "Storming of the Crag",               "The Crag",        600),
+        /* 13 */mkexcursion(mkdate(14,  3,  299),  1, "Red Wedding",                        "The Twins",      1399),
+        /* 14 */mkexcursion(mkdate(17,  8,  299),  2, "Fall of Astapor",                    "Astapor",         750),
+        /* 15 */mkexcursion(mkdate( 9, 11,  299),  3, "Siege of Meereen",                   "Meereen",         999),
         
-        // Availale excursions
-        ExcursionDTO e1 = new ExcursionDTO();
-        ExcursionDTO e2 = new ExcursionDTO();
-        ExcursionDTO e3 = new ExcursionDTO();
-        e1.setDestination("Afgánistán");
-        e1.setDescription("Best of war");
-        e1.setDuration(3);
-        e1.setExcursionDate(mkdate(25, 6, 2014));
-        e1.setPrice(new BigDecimal(150));
-        e2.setDestination("Afgánistán");
-        e2.setDescription("Best of army stock");
-        e2.setDuration(2);
-        e2.setExcursionDate(mkdate(26, 6, 2014));
-        e2.setPrice(new BigDecimal(10));
-        e3.setDestination("Afgánistán");
-        e3.setDescription("Best places on cemetary");
-        e3.setDuration(1);
-        e3.setExcursionDate(mkdate(30, 6, 2014));
-        e3.setPrice(new BigDecimal(5));
-        excursionService.addExcursion(e1);
-        excursionService.addExcursion(e2);
-        excursionService.addExcursion(e3);
+        // Special offer "Wherever the Wind Takes Me"
+        /* 16 */mkexcursion(mkdate(17, 10, 2015),  2, "Surprise Excursion 1",                  "Location 1",  1300),
+        /* 17 */mkexcursion(mkdate(24, 11, 2015),  7, "Surprise Excursion 2",                  "Location 2",  3200),
+        /* 18 */mkexcursion(mkdate( 5, 12, 2015),  4, "Surprise Excursion 3",                  "Location 3",  1499),
+        /* 19 */mkexcursion(mkdate(19, 12, 2015),  3, "Surprise Excursion (iPad owners only)", "Location 4", 24399)
+        );
+        
+        for (ExcursionDTO excursion : excursions) {
+            excursionService.addExcursion(excursion);
+        }
+        
+        //----------------------------------------------------------------------
+        //  Trips
+        //----------------------------------------------------------------------
+        
+        trips = Arrays.asList(
+        /* 00 */mktrip(mkdate(15, 10, 2941), mkdate(25, 10, 2941), "Erebor",                      50,  7399,           0),
+        /* 01 */mktrip(mkdate(30,  1, 3017), mkdate(15,  7, 3019), "Middle-Earth",               100, 14999, range(2, 9)),
+        /* 02 */mktrip(mkdate(12,  3, 3019), mkdate( 5,  5, 3019), "Gondor",                     100,  3999,       6,  8),
+        /* 03 */mktrip(mkdate( 6,  1,    2), mkdate(12,  1,    2), "Ancient Westeros",            75,  1999,          10),
+        /* 04 */mktrip(mkdate( 1,  1,  299), mkdate(16,  3,  299), "Westeros",                   150,  4590,      12, 13),
+        /* 05 */mktrip(mkdate(15,  8,  299), mkdate(15, 11,  299), "Essos",                       60,  2400,      14, 15),
+        /* 06 */mktrip(mkdate(15, 10, 2015), mkdate(30, 12, 2015), "Wherever The Wind Takes Me", 100, 9999, range(16, 19))
+        );
+        
+        for (TripDTO trip : trips) {
+            tripService.addTrip(trip);
+        }
+        
+        //----------------------------------------------------------------------
+        //  Customers
+        //----------------------------------------------------------------------
+        
+        customers = Arrays.asList(
+        /* 00 */mkcustomer("Gandalf the Grey",   "All Over Middle-Earth"),
+        /* 01 */mkcustomer("Gandalf the White",  "All Over Middle-Earth"),
+        /* 02 */mkcustomer("Bilbo Baggins",      "Bag End"),
+        /* 03 */mkcustomer("Frodo Baggins",      "Bag End"),
+        /* 04 */mkcustomer("Aragorn",            "Minas Tirith"),
+        /* 05 */mkcustomer("Aegon I Targaryen",  "Dragonstone"),
+        /* 06 */mkcustomer("Robb Start",         "Winterfell"),
+        /* 07 */mkcustomer("Daenerys Targaryen", "Vaes Dothrak")
+        );
+        
+        for (CustomerDTO customer : customers) {
+            customerService.addCustomer(customer);
+        }
+        
+        //----------------------------------------------------------------------
+        //  Accounts
+        //----------------------------------------------------------------------
+        
+        accounts = Arrays.asList(
+        /* 00 */mkaccount("gandalf",  "mithrandir",   UserRole.ROLE_STAFF, 0),
+        /* 01 */mkaccount("gandalfw", "incanus",      UserRole.ROLE_ADMIN, 1),
+        /* 02 */mkaccount("bilbo",    "arkenstone",   UserRole.ROLE_USER,  2),
+        /* 03 */mkaccount("frodo",    "the_ring",     UserRole.ROLE_USER,  3),
+        /* 04 */mkaccount("aragorn",  "telcontar",    UserRole.ROLE_STAFF, 4),
+        /* 05 */mkaccount("aegon",    "balerion",     UserRole.ROLE_USER,  5),
+        /* 06 */mkaccount("robb",     "glass_garden", UserRole.ROLE_USER,  6),
+        /* 07 */mkaccount("dany",     "stormborn",    UserRole.ROLE_ADMIN, 7)
+        );
+        
+        for (AccountDTO account : accounts) {
+            accountService.addAccount(account);
+        }
+        
+        //----------------------------------------------------------------------
+        //  Reservations
+        //----------------------------------------------------------------------
+        
+        reservations = Arrays.asList(
+        //              C  T    Excursions
+        /* 00 */mkresrv(0, 0,    0),
+        /* 01 */mkresrv(0, 1,    2,  4,  6),
+        /* 02 */mkresrv(1, 1,    8),
+        /* 03 */mkresrv(2, 0,    0),
+        /* 04 */mkresrv(3, 1,    5,  7,  8),
+        /* 05 */mkresrv(4, 2,    6,  8),
+        /* 06 */mkresrv(5, 3,   10),
+        /* 07 */mkresrv(6, 4,   12, 13),
+        /* 08 */mkresrv(7, 5,   14, 15)
+        );
+        
+        for (ReservationDTO reservation : reservations) {
+            reservationService.addReservation(reservation);
+        }
 
-        List<ExcursionDTO> excursions = Arrays.asList(
-            mkexcursion(mkdate(20, 10, 2941), 2, "Battle of Five Armies",   "Erebor",      new BigDecimal(500)),
-            mkexcursion(mkdate(25, 10, 3018), 1, "Council of Elrond",       "Rivendell",   new BigDecimal(150)),
-            mkexcursion(mkdate(02, 03, 3019), 1, "Destruction of Isengard", "Isengard",    new BigDecimal(400)),
-            mkexcursion(mkdate(03, 03, 3019), 1, "Battle of Hornburg",      "Helm's Deep", new BigDecimal(350)),
-            mkexcursion(mkdate(14, 03, 3019), 3, "Mt Doom Excursion",       "Mordor",      new BigDecimal(200)),
-            mkexcursion(mkdate(25, 03, 3019), 2, "Downfall of Barad-dûr",   "Mordor",      new BigDecimal(300))
-        );
-        
-        for(ExcursionDTO e : excursions) {
-            excursionService.addExcursion(e);
-        }
-        
-        List<TripDTO> trips = Arrays.asList(
-            mktrip(mkdate(11, 10, 2960), mkdate(18, 11, 2960), "Wonderful Moria",   20, new BigDecimal(800)),
-            mktrip(mkdate(24, 10, 3018), mkdate(25, 03, 3019), "The Life of Frodo", 45, new BigDecimal(900))
-        );
-        
-        trips.get(1).setExcursions(excursions.subList(1, 5));
-        
-        for(TripDTO t : trips) {
-            tripService.addTrip(t);
-        }
-        
-        // Available trips
-        // TODO, there is just one for reservation, can be removed l8r
-        TripDTO t1 = new TripDTO();       
-        t1.setDateFrom(mkdate(21, 6, 2014));
-        t1.setDateTo(mkdate(3, 7, 2014));
-        t1.setDestination("Biking in Haradwaith regions");
-        t1.setCapacity(15);
-        t1.setBasePrice(new BigDecimal(11999));
-        t1.setExcursions(Arrays.asList(new ExcursionDTO[] {e1, e2, e3}));
-        tripService.addTrip(t1);
-        
-        // Available reservations
-        ReservationDTO r1 = new ReservationDTO();
-        r1.setTrip(t1);
-        r1.addExcursion(e1);
-        r1.addExcursion(e2);
-        r1.setCustomer(c1);
-        reservationService.addReservation(r1);
-        
-        CustomerDTO customer1 = mkcustomer("Customer attached to USER",  null);
-        CustomerDTO customer2 = mkcustomer("Customer attached to STAFF", null);
-        CustomerDTO customer3 = mkcustomer("Customer attached to ADMIN", null);
-        
-        customerService.addCustomer(customer1);
-        customerService.addCustomer(customer2);
-        customerService.addCustomer(customer3);
-        
-        List<AccountDTO> testaccounts = Arrays.asList(
-            mkaccount("user",   "user",   UserRole.ROLE_USER,  null),
-            mkaccount("userc",  "userc",  UserRole.ROLE_USER,  customer1),
-            mkaccount("staff",  "staff",  UserRole.ROLE_STAFF, null),
-            mkaccount("staffc", "staffc", UserRole.ROLE_STAFF, customer2),
-            mkaccount("admin",  "admin",  UserRole.ROLE_ADMIN, null),
-            mkaccount("adminc", "adminc", UserRole.ROLE_ADMIN, customer3)
-        );
-        
-        for(AccountDTO pewpew : testaccounts) {
-            accountService.addAccount(pewpew);
-        }
-        
-        AccountDTO rest = new AccountDTO();
-        rest.setUsername("rest");
-        rest.setPlainPassword("dummy-plain-password");
-        rest.setPassword(encoder.encode("rest"));
-        rest.setRoles(EnumSet.of(UserRole.ROLE_USER));
-        accountService.addAccount(rest);
+        //----------------------------------------------------------------------
+        //  Logout the root
+        //----------------------------------------------------------------------
         
         SecurityContextHolder.getContext().getAuthentication().setAuthenticated(false);
     }
